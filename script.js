4b9fbd7c2c8dd7afa39512be2b51b6d195a8f569
Samuel A <al2193001733@azc.uam.mx>
2022-06-01 00:58:31 -0500

    add keyboard to equal and numbers

--- script.js
+++ script.js
@@ -1,62 +1,119 @@
 let number = 0;
 let op = "";
+let numerator = document.querySelector(".output");
+let denominator = document.querySelector(".in");
 
-initialize = () =>{
+
+initialize = () => {
+    let equalBtn = document.querySelector(".equals");
+    let percentBtn = document.querySelector(".percent");
+    let acBtn = document.querySelector(".AC");
+    let backspaceBtn = document.querySelector(".backspace");
+
     let nums = document.querySelectorAll(".num");
     let operators = document.querySelectorAll(".operator");
-    nums.forEach((num) => {
-        num.addEventListener("click", ()=>{
-            document.querySelector(".in").textContent += num.textContent;
-        });
-    });
-    
-    operators.forEach((operator) => {
-        operator.addEventListener("click", () =>{
-            if(document.querySelector(".in").textContent != ""){
-                document.querySelector(".output").textContent  = (document.querySelector(".in").textContent);
-                document.querySelector(".in").textContent = "";
-            }
-            op = operator.textContent;
-        });
-    });
 
-    let equal = document.querySelector(".equals");
-    equal.addEventListener("click", () =>{
-        if(document.querySelector(".in").textContent != "" ){
-            console.log(document.querySelector(".output").textContent + " " + op + " " + document.querySelector(".in").textContent);
+
+    let numInsert = function (val) {
+        if (val.type === "click") {
+            denominator.textContent += this.textContent;
+        }
+        else {
+            denominator.textContent += val.key;
+        }
+
+    };
+
+    let operatorSelect = function () {
+        if (denominator.textContent !== "") {
+            numerator.textContent = denominator.textContent;
+            denominator.textContent = "";
+        }
+        op = this.textContent;
+    };
+
+    let solve = function () {
+        if (denominator.textContent !== "") {
+            //console.log(numerator.textContent + " " + op + " " + denominator.textContent);
             switch (op) {
                 case "+":
-                    document.querySelector(".output").textContent = parseFloat(document.querySelector(".output").textContent) + parseFloat(document.querySelector(".in").textContent);
+                    numerator.textContent = parseFloat(numerator.textContent) + parseFloat(denominator.textContent);
                     break;
                 case "-":
-                    document.querySelector(".output").textContent = parseFloat(document.querySelector(".output").textContent) - parseFloat(document.querySelector(".in").textContent);
+                    numerator.textContent = parseFloat(numerator.textContent) - parseFloat(denominator.textContent);
                     break;
                 case "*":
-                    document.querySelector(".output").textContent = parseFloat(document.querySelector(".output").textContent) * parseFloat(document.querySelector(".in").textContent);
+                    numerator.textContent = parseFloat(numerator.textContent) * parseFloat(denominator.textContent);
                     break;
                 case "/":
-                    document.querySelector(".output").textContent = parseFloat(document.querySelector(".output").textContent) / parseFloat(document.querySelector(".in").textContent);
+                    numerator.textContent = parseFloat(numerator.textContent) / parseFloat(denominator.textContent);
+                    break;
+
+                default:
+                    numerator.textContent = denominator.textContent;
                     break;
             }
             op = "";
-            document.querySelector(".in").textContent = "";
-            
-        }         
-    })
 
-    let backspace = document.querySelector(".backspace");
+            denominator.textContent = "";
+        };
+    };
 
-    backspace.addEventListener("click" , () =>{
-        document.querySelector(".in").textContent =  document.querySelector(".in").textContent.slice(0,-1);
+
+    //add click event for each number
+    nums.forEach((num) => {
+        num.addEventListener("click", numInsert);
     });
 
-    let ac = document.querySelector(".AC");
-    ac.addEventListener("click" , () =>{
-        document.querySelector(".in").textContent = "";
-        document.querySelector(".output").textContent = "";
+
+    //add click event for operator selected
+    operators.forEach((operator) => {
+        operator.addEventListener("click", operatorSelect);
+    });
+
+    // click event for equal button
+    equalBtn.addEventListener("click", solve);
+
+    //events for backspace button, clear all and percent btn
+    backspaceBtn.addEventListener("click", () => {
+        denominator.textContent = denominator.textContent.slice(0, -1);
+    });
+
+    acBtn.addEventListener("click", () => {
+        denominator.textContent = "";
+        numerator.textContent = "";
         op = "";
     });
-    
+
+    percentBtn.addEventListener("click", () => {
+        if (denominator.textContent === "") {
+            numerator.textContent = parseFloat(numerator.textContent) / 100;
+        }
+        else {
+            denominator.textContent = parseFloat(denominator.textContent) / 100;
+        }
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key.match(/[0-9]/g)) {
+            numInsert(e);
+        }
+
+        else if (e.key.match(/ [*+-/] /g)) {
+            operatorSelect(e);
+        }
+
+        else if (e.key === "Backspace") {
+            denominator.textContent = denominator.textContent.slice(0, -1);
+        }
+
+        if (e.key === "Enter" || e.key === "=") {
+            e.preventDefault();
+            solve();
+
+        }
+    });
+
 };
 
 initialize();
